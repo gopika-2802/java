51af3bbddc247f63736caab6ff48e308a8a37517
Gopika Govindaraj <gopikagovindaraj2802@gmail.com>
2026-07-31 10:42:12 +0530

    Add diagonal sum of 2D matrix

---
+++ Array/diagonalsum_2D_matrix.java
@@ -0,0 +1,18 @@
+package Array;
+public class diagonalsum_2D_matrix {
+    public static void main(String[] args) {
+        int mat[][]={{1,2,3},{4,5,6},{7,8,9}};
+        int sum=0;
+        for(int i=0;i<mat.length;i++)
+        {
+            for(int j=0;j<mat[0].length;j++)
+            {
+                if(i==j)
+                    sum+=mat[i][j];
+                 else if(i+j==mat.length-1)
+                    sum+=mat[i][j];
+            }
+        }
+        System.out.println(sum);
+    }
+}
