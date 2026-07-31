af81fd34f4e2c2264248ebc7108de34623630898
Gopika Govindaraj <gopikagovindaraj2802@gmail.com>
2026-07-31 16:29:53 +0530

    Add display 1 to 100 program

---
+++ DAY1/Display_1to100.java
@@ -0,0 +1,14 @@
+package DAY1;
+
+//import java.util.Scanner;
+
+public class Display_1to100 {
+    public static void main(String[] args) {
+        //Scanner sc=new Scanner(System.in);
+        //int n=sc.nextInt();
+        for(int i=1;i<=100;i++)
+        System.out.print(i+" ");
+        //sc.close();
+    }
+    
+}
