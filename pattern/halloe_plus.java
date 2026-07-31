ae48f61c7b2f2849ce20fb7a2eb32ce2c93fa6bb
Gopika Govindaraj <gopikagovindaraj2802@gmail.com>
2026-07-31 16:21:49 +0530

    Add halloe plus pattern

---
+++ pattern/halloe_plus.java
@@ -0,0 +1,27 @@
+package pattern;
+
+import java.util.Scanner;
+
+public class halloe_plus {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                if(i==1||i==n||j==1||j==n||i==(n+1)/2||j==(n+1)/2)
+                {
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.print("\n");
+        }
+        sc.close();
+    }
+
+    
+}
