0f7a481c62ef73a3a56b8d9d15d8c3dc07bb0fa0
Gopika Govindaraj <gopikagovindaraj2802@gmail.com>
2026-07-31 21:06:40 +0530

    Add remove spaces

---
+++ Strings/RemoveSpaces.java
@@ -0,0 +1,16 @@
+package Strings;
+import java.util.*;
+
+class RemoveSpaces {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != ' ') {
+                System.out.print(s.charAt(i));
+            }
+        }
+    }
+}
