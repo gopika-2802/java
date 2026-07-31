b7fae71b4cfe98aae4d493f7d6e110f2b3be8560
Gopika Govindaraj <gopikagovindaraj2802@gmail.com>
2026-07-31 10:43:42 +0530

    Add insert element in array

---
+++ Array/InsertelementinArray.java
@@ -0,0 +1,32 @@
+package Array;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class InsertelementinArray {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        //int n=sc.nextInt();
+        //int [] arr=new int[n];
+        /*for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }*/
+        int arr[]={1,2,4,5,6};
+        int n=arr.length;
+        int arrn[]=new int [n+1];
+        System.out.println("enter the element to insert :");
+        int ele=sc.nextInt();
+        System.out.println("enter the index to insert: ");
+        int ind=sc.nextInt();
+        for(int i=0;i<ind;i++)
+            arrn[i]=arr[i];
+        arrn[ind]=ele;
+        for(int i=ind;i<n;i++)
+            arrn[i+1]=arr[i];
+        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(arrn));
+        sc.close();
+    }
+    
+}
