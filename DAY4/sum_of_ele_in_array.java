66450a5f1fd78389d5f391e8e3bcc1341f0cd68e
Gopika Govindaraj <gopikagovindaraj2802@gmail.com>
2026-07-31 19:45:25 +0530

    Add sum of array elements

---
+++ DAY4/sum_of_ele_in_array.java
@@ -0,0 +1,11 @@
+package DAY4;
+
+public class sum_of_ele_in_array {
+    public static void main(String[] args) {
+        int [] arr={1,5,3,9,7};
+        int s=0;
+        for(int i=0;i<arr.length;i++)
+        s=s+arr[i];
+        System.out.println(s);
+    }
+}
