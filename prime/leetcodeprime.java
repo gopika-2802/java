34bc4ba46dcb43df7d5aca92f26831083cd968ab
Gopika Govindaraj <gopikagovindaraj2802@gmail.com>
2026-07-31 16:22:35 +0530

    Add LeetCode prime solution

---
+++ prime/leetcodeprime.java
@@ -0,0 +1,30 @@
+package prime;
+
+import java.util.Arrays;
+
+public class leetcodeprime {
+    class Solution {
+    public int countPrimes(int n) {
+        if (n <= 2) return 0;
+
+    boolean[] isPrime = new boolean[n];
+    Arrays.fill(isPrime, true);
+    isPrime[0] = false;
+    isPrime[1] = false;
+    for (int i = 2; i * i < n; i++) {
+        if (isPrime[i]) {
+            for (int j = i * i; j < n; j += i) {
+                isPrime[j] = false;
+            }
+        }
+    }
+
+    int count = 0;
+    for (boolean p : isPrime) {
+        if (p) count++;
+    }
+    return count;
+    }
+}
+    
+}
