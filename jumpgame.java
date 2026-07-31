a4c3fe00b507682acf87b6b1624af0f607b8d5f8
Gopika Govindaraj <gopikagovindaraj2802@gmail.com>
2026-07-31 14:45:30 +0530

    Add jump game solution

---
+++ jumpgame.java
@@ -0,0 +1,13 @@
+public class jumpgame{
+    public static int jump(int arr[]){
+      for(int i=0;i<arr.length;i++){
+        
+      }
+      return 0;
+    }
+    public static void main(String[] args){
+    int[] arr={1,2,1,1,1};
+    int ans=jump(arr);
+    System.out.println(ans);
+    }
+}
