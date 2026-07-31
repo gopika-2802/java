d43ebd7ab373614ae0fabb2567328fac977165c7
Gopika Govindaraj <gopikagovindaraj2802@gmail.com>
2026-07-31 14:49:57 +0530

    Add even odd without division

---
+++ evenoroddwithoutdiv.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class evenoroddwithoutdiv {
+    public static void main(String[] args) 
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println((n&1)==0?"even":"odd");
+        sc.close();
+    }
+    
+}
