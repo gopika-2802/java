42bf28fb373c13acffb8f4761344cb19c0b5bc5e
Gopika Govindaraj <gopikagovindaraj2802@gmail.com>
2026-07-31 10:38:37 +0530

    Add array rotation using reverse

---
+++ Array/Arrayrotationusingreverse.java
@@ -0,0 +1,35 @@
+package Array;
+
+import java.util.Arrays;
+
+public class Arrayrotationusingreverse {
+    public static void reverse(int []arr,int s,int e)
+    {
+        while(s<=e)
+        {
+            int temp=arr[s];
+            arr[s]=arr[e];
+            arr[e]=temp;
+            s++;
+            e--;
+        }
+    }
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5,6};
+        int k=2;
+        int n=arr.length;
+        k=k%n;
+        //left rotation
+        /*
+        reverse(arr,0,k-1);
+        reverse(arr,k,n-1);
+        reverse(arr, 0, n-1);
+         */
+        reverse(arr, 0, n-1);
+        reverse(arr,0,k-1);
+        reverse(arr,k,n-1);
+        System.out.println(Arrays.toString(arr));
+
+    }
+    
+}
